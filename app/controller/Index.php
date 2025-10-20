f922e6b434d7abc8cffc2963e06892bef0b20c56
34234234@qq.com <34234234@qq.com>
2025-10-20 18:20:05 +0800

    a

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -18,5 +18,18 @@ class Index extends BaseController
         $body = (string)$response->getBody();
         $body = json_decode($body,true);
         print_r($body);
+
+
+        echo "<hr/>";
+        $apikey = config('cms.api_key_q');
+        $url = 'https://api.telegram.org/bot'.$apikey.'/getWebhookInfo';
+        $client = new Client();
+        $response = $client->request('POST', $url, [
+            'verify' => false,
+        ]);
+        $body = (string)$response->getBody();
+        $body = json_decode($body,true);
+
+        var_dump('api_key_q',$body);
     }
 }
