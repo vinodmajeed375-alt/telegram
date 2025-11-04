2346bb603e20f1dc0ae3ceb04780c00023f7a65e
34234234@qq.com <34234234@qq.com>
2025-11-04 17:19:53 +0800

    a

--- app/controller/Telegramhash.php
+++ app/controller/Telegramhash.php
@@ -4,6 +4,10 @@ use app\BaseController;
 use danog\MadelineProto\API;
 use danog\MadelineProto\Exception;
 use danog\MadelineProto\Settings;
+use danog\MadelineProto\LocalFile;
+use danog\MadelineProto\ParseMode;
+use Longman\TelegramBot\Entities\InputMedia\InputMediaPhoto;
+
 
 class Telegramhash extends BaseController
 {
@@ -16,6 +20,7 @@ class Telegramhash extends BaseController
         $session_file = 'session.madeline'; // 会话文件路径
         $MadelineProto = new API($session_file, $settings);
 
+
         try {
             $MadelineProto->start(); // 启动会话，首次运行会提示验证
         } catch (Exception $e) {
@@ -24,62 +29,39 @@ class Telegramhash extends BaseController
         }
 
 
-
-
         $peer_id = -4663428801; // 目标群组或频道ID
-
-        $file_id = 'AgACAgUAAxkBAAIXfWkJsVzDBfSjgZKoUFF5ra2WFaRGAAICDGsbJkhJVImkTYpUa_RCAQADAgADeQADNgQ'; // 图片文件路径或URL，也可以是媒体ID
         $file_id = 'AgACAgUAAxkBAAIXfWkJsVzDBfSjgZKoUFF5ra2WFaRGAAICDGsbJkhJVImkTYpUa_RCAQADAgADeAADNgQ';
 
+        $file_path =realpath(public_path().'/2025050804415560.webp');
 
-//        $upload = $MadelineProto->upload([
-//            'file' => public_path() . '/luotuo-001.jpg',
-//            //'file' => 'https://img.ivsky.com/img/tupian/t/202108/25/luotuo-001.jpg',
-//        ]);
-//
-//        // 获取正式的file_id
-//        $file_info = $MadelineProto->getFile($upload['file_id']);
-//        $file_id = $file_info['file_id'];
-//
-//        var_dump($file_id);
-
+        if ($file_path === false || !file_exists($file_path)) {
+            die('文件不存在或路径错误：' . $file_path);
+        }
+        $media = new LocalFile($file_path);
 
         $caption = 'xx这是图片的说';
-
-//        try {
-//            $chats = $MadelineProto->getChat($peer_id);
-//            if (!isset($chats)) {
-//                throw new \Exception("目标聊天未导入");
-//            }
-//        } catch (\Exception $e) {
-//            echo '导入聊天失败：', $e->getMessage(), "\n";
-//        }
-
         try {
 
-//            $MadelineProto->messages->sendMessage([
-//                'peer' => $peer_id,
-//                'message' => $caption,
-//            ]);
-
-            $MadelineProto->messages->sendMedia([
+            $result = $MadelineProto->messages->sendMedia([
                 'peer' => $peer_id,
-                'media' => $file_id,
+                'media' => $media,
                 'caption' => $caption,
             ]);
-
-            $MadelineProto->messages->sendMedia([
-                'peer' => -4724604306,
-                'media' => $file_id,
+            $result = $MadelineProto->messages->sendMedia([
+                'peer' => $peer_id,
+                'media' => $file_path,
                 'caption' => $caption,
             ]);
-
-
+         
             echo "图片消息已发送！";
         } catch (\Exception $e) {
             echo '发送失败：', $e->getMessage();
         }
 
+//            $MadelineProto->messages->sendMessage([
+//                'peer' => $peer_id,
+//                'message' => $caption,
+//            ]);
 
     }
 }
