495d59ce143aeed5ccae914adb3504d593b3f470
34234234@qq.com <34234234@qq.com>
2025-11-04 16:21:15 +0800

    a

--- app/controller/Telegramhash.php
+++ app/controller/Telegramhash.php
@@ -28,28 +28,49 @@ class Telegramhash extends BaseController
 
         $peer_id = -4663428801; // 目标群组或频道ID
 
-        $media = 'https://cdn.pixabay.com/photo/2025/10/17/15/16/halloween-9900545_1280.jpg'; // 图片文件路径或URL，也可以是媒体ID
-        $caption = '这是图片的说明';
+        $file_id = 'AgACAgUAAxkBAAIXfWkJsVzDBfSjgZKoUFF5ra2WFaRGAAICDGsbJkhJVImkTYpUa_RCAQADAgADeQADNgQ'; // 图片文件路径或URL，也可以是媒体ID
+        $file_id = 'AgACAgUAAxkBAAIXfWkJsVzDBfSjgZKoUFF5ra2WFaRGAAICDGsbJkhJVImkTYpUa_RCAQADAgADeAADNgQ';
+
+
+        $upload = $MadelineProto->upload([
+            'file' => public_path() . '/luotuo-001.jpg',
+            //'file' => 'https://img.ivsky.com/img/tupian/t/202108/25/luotuo-001.jpg',
+        ]);
+
+        // 获取正式的file_id
+        $file_info = $MadelineProto->getFile($upload['file_id']);
+        $file_id = $file_info['file_id'];
+
+        var_dump($file_id);
+
+
+        $caption = 'xx这是图片的说';
+
+//        try {
+//            $chats = $MadelineProto->getChat($peer_id);
+//            if (!isset($chats)) {
+//                throw new \Exception("目标聊天未导入");
+//            }
+//        } catch (\Exception $e) {
+//            echo '导入聊天失败：', $e->getMessage(), "\n";
+//        }
 
         try {
-            $chats = $MadelineProto->getChat($peer_id);
-            if (!isset($chats)) {
-                throw new \Exception("目标聊天未导入");
-            }
-        } catch (\Exception $e) {
-            echo '导入聊天失败：', $e->getMessage(), "\n";
-        }
 
-        try {
+//            $MadelineProto->messages->sendMessage([
+//                'peer' => $peer_id,
+//                'message' => $caption,
+//            ]);
+
             $MadelineProto->messages->sendMedia([
                 'peer' => $peer_id,
-                'media' => $media,
+                'media' => $file_id,
                 'caption' => $caption,
             ]);
 
             $MadelineProto->messages->sendMedia([
                 'peer' => -4724604306,
-                'media' => $media,
+                'media' => $file_id,
                 'caption' => $caption,
             ]);
 
