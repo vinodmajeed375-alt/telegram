9f296c9c640f8f0c11a9477052bf5e9c3ee26522
34234234@qq.com <34234234@qq.com>
2025-11-04 15:22:22 +0800

    a

--- app/controller/Telegramhash.php
+++ app/controller/Telegramhash.php
@@ -24,33 +24,30 @@ class Telegramhash extends BaseController
         }
 
         $peer_id = -4919097926; // 目标群组或频道ID
-        $media = 'AgACAgUAAxkBAAIXKGkIZCJvkTW1daOPqWge_BnQqQUVAALfC2sbH5BAVNv6BbNlE-o5AQADAgADcwADNgQ'; // 媒体ID
-        $caption = '图片说明';
 
-        // 先确保目标聊天已加载（用 getChats()）
-        try {
-            // 获取目标聊天信息，确保它存在
-            $chats = $MadelineProto->getChat($peer_id);
-           // $chats = $MadelineProto->getChats([$peer_id]);
-            if (!isset($chats[$peer_id])) {
-                throw new \Exception("目标聊天不存在或未导入");
-            }
-        } catch (Exception $e) {
-            echo '获取聊天信息失败：', $e->getMessage(), "\n";
-            exit;
-        }
+        $media = 'https://cdn.pixabay.com/photo/2025/10/17/15/16/halloween-9900545_1280.jpg'; // 图片文件路径或URL，也可以是媒体ID
+        $caption = '这是图片的说明';
 
-        // 发送媒体消息
         try {
             $MadelineProto->messages->sendMedia([
                 'peer' => $peer_id,
                 'media' => $media,
                 'caption' => $caption,
             ]);
-            echo "消息已发送！";
-        } catch (Exception $e) {
-            echo '发送消息失败：', $e->getMessage();
+
+            $MadelineProto->messages->sendMedia([
+                'peer' => -4724604306,
+                'media' => $media,
+                'caption' => $caption,
+            ]);
+
+
+            echo "图片消息已发送！";
+        } catch (\Exception $e) {
+            echo '发送失败：', $e->getMessage();
         }
+
+
     }
 }
 ?>
